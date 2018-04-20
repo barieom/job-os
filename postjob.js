15e5a34fc74199fb3c3f34983e799af0439f8426
juliahedrick <julia.hedrick3@gmail.com>
2018-04-19 21:58:31 -0400

    worked on post request for postjob page

--- postjob.js
+++ postjob.js
@@ -1,19 +1,25 @@
 function postJobSubmit() {
 	var jobTitle = document.getElementById("jobTitle").value;
-	var department = document.getElementById("department").value;; 
-	var jobDescript = document.getElementById("jobDescript").value;;
-	var WSoS = document.getElementById("WSoS").value;;
-	var hourlyRate = document.getElementById("hourlyRate").value;;
-	var hoursPerWeek = document.getElementById("hoursPerWeek").value;;
-	var link = document.getElementById("link").value;;
+	var department = document.getElementById("department").value; 
+	var jobDescript = document.getElementById("jobDescript").value;
+	var WSoS = document.getElementById("WSoS").value;
+	var hourlyRate = document.getElementById("hourlyRate").value;
+	var hoursPerWeek = document.getElementById("hoursPerWeek").value;
+	var link = document.getElementById("link").value;
 
-	console.log(jobTitle);
-	console.log(department);
-	console.log(jobDescript);
-	console.log(WSoS);
-	console.log(hourlyRate);
-	console.log(hoursPerWeek);
-	console.log(link);
+	document.getElementById("jobTitle").value = "";
+	document.getElementById("department").value = ""; 
+	document.getElementById("jobDescript").value = "";
+	document.getElementById("WSoS").value = "unspecified";
+	document.getElementById("hourlyRate").value = "";
+	document.getElementById("hoursPerWeek").value = "";
+	document.getElementById("link").value = "";
 
+	params = "jobTitle=" + jobTitle + "&department=" + department + "&jobDescript=" +  jobDescript + "&WSoS=" + WSoS + "&hourlyRate=" + hourlyRate + "&hoursPerWeek=" + hoursPerWeek + "&link=" + link;
+	///update URL to right one
+	url    = "https://job-os.herokuapp.com/postJob";
 
+	request = new XMLHttpRequest();
+	request.open("POST", url, true);
+	request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 }
