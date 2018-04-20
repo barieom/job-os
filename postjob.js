86f6b1c534d804fab46f1882d8f62e1ad89424b1
juliahedrick <julia.hedrick3@gmail.com>
2018-04-19 21:39:39 -0400

    adding Javascript to forms

---
+++ postjob.js
@@ -0,0 +1,19 @@
+function postJobSubmit() {
+	var jobTitle = document.getElementById("jobTitle").value;
+	var department = document.getElementById("department").value;; 
+	var jobDescript = document.getElementById("jobDescript").value;;
+	var WSoS = document.getElementById("WSoS").value;;
+	var hourlyRate = document.getElementById("hourlyRate").value;;
+	var hoursPerWeek = document.getElementById("hoursPerWeek").value;;
+	var link = document.getElementById("link").value;;
+
+	console.log(jobTitle);
+	console.log(department);
+	console.log(jobDescript);
+	console.log(WSoS);
+	console.log(hourlyRate);
+	console.log(hoursPerWeek);
+	console.log(link);
+
+
+}
