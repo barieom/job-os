c0c46c75f9c06e6377e8589a27839240b55b129f
Barry Eom <31739208+barieom@users.noreply.github.com>
2018-04-30 00:19:13 -0400

    fixed link from findjob

--- findjob.js
+++ findjob.js
@@ -1,30 +1,3 @@
-function findJobInit() {
-	request = new XMLHttpRequest();
-	request.open("GET", "https://job-os.herokuapp.com/jobInfo", true);
-
-	request.onreadystatechange = function() {
-		if (request.readyState == 4 && request.status == 200) {
-			outputString = document.getElementById("AvailJobs");
-			reqObj = request.responseText;				
-
-			avail_jobs = JSON.parse(reqObj);
-			string = "";
-
-			for (count = 0; count < avail_jobs.jobinfo.length; count++) {
-				var title = avail_jobs.jobinfo[count].jobTitle;
-				if (count%2==0) {
-					string += "<div class='bg_grey fill-div'><a href='https://job-os.herokuapp.com/markerLink.html?jobTitle='" + title + "'/>" + "<div class='black_text'>" + "<p class='no_margin'> Job Title: " + title + "</p><p class='no_margin'> Department: " + avail_jobs.jobinfo[count].department + "</p> <p class='no_margin'> Hourly Rate: " + avail_jobs.jobinfo[count].hourlyRate +  "</p>"+ "</div></div>";
-				} else {
-					string += "<div class='bg_white fill-div'><a href='https://job-os.herokuapp.com/markerLink.html?jobTitle='" + title + "'/>" + "<div class='black_text'>" + "<p class='no_margin'> Job Title: " + title + "</p><p class='no_margin'> Department: " + avail_jobs.jobinfo[count].department + "</p> <p class='no_margin'> Hourly Rate: " + avail_jobs.jobinfo[count].hourlyRate +  "</p>"+ "</div></div>";
-				}
-			}	
-
-			outputString.innerHTML = string;
-		} 
-	};
-	request.send();
-}
-
 function getJobs() {
 	var request1 = new XMLHttpRequest();
 	request1.open("GET", "https://job-os.herokuapp.com/jobInfo", true);
@@ -40,12 +13,15 @@ function getJobs() {
 			for (count = 0; count < avail_jobs.jobinfo.length; count++) {
 				var title = avail_jobs.jobinfo[count].jobTitle;	
 				var WSoS = avail_jobs.jobinfo[count].WSoS;
+				var url = "https://job-os.herokuapp.com/markerLink.html?jobTitle="+title;
+				
 				if (WSoS == "Work Study") { WSoS = "Yes"}
-				else if (WSoS == "N/A" || WSoS == "Standard") {WSoS = "Standard"}	
+				else if (WSoS == "N/A" || WSoS == "Standard") {WSoS = "Standard"};
+
 				if (count%2==0) {
-					string += "<div class='bg_grey fill-div'><a href='https://job-os.herokuapp.com/markerLink.html?jobTitle='" + title + "'/>" + "<div class='black_text row'>" + "<div class='job_title no_margin col-4'>" + title + "</div><div class='col-8 no_margin'> Affiliation: " + avail_jobs.jobinfo[count].department + "<br> Hourly Rate: " + avail_jobs.jobinfo[count].hourlyRate + "<br> Work Study: " + WSoS + "</div></div></div>";
+					string += "<div class='bg_grey fill-div'><a href=" + url + ">" + "<div class='black_text row'>" + "<div class='job_title no_margin col-4'>" + title + "</div><div class='col-8 no_margin'> Affiliation: " + avail_jobs.jobinfo[count].department + "<br> Hourly Rate: " + avail_jobs.jobinfo[count].hourlyRate + "<br> Work Study: " + WSoS + "</div></div></div>";
 				} else {
-					string += "<div class='bg_white fill-div'><a href='https://job-os.herokuapp.com/markerLink.html?jobTitle='" + title + "'/>" + "<div class='black_text row'>" + "<div class='job_title no_margin col-4'>" + title + "</div><div class='col-8 no_margin'> Affiliation: " + avail_jobs.jobinfo[count].department + "<br> Hourly Rate: " + avail_jobs.jobinfo[count].hourlyRate + "<br> Work Study: " + WSoS + "</div></div></div>";
+					string += "<div class='bg_white fill-div'><a href=" + url + ">" + "<div class='black_text row'>" + "<div class='job_title no_margin col-4'>" + title + "</div><div class='col-8 no_margin'> Affiliation: " + avail_jobs.jobinfo[count].department + "<br> Hourly Rate: " + avail_jobs.jobinfo[count].hourlyRate + "<br> Work Study: " + WSoS + "</div></div></div>";
 				}			
 			}
 			outputString.innerHTML = string;
