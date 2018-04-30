4b1f1aee68fe6133a5690c30ad6920369e7bc608
juliahedrick <julia.hedrick3@gmail.com>
2018-04-29 21:45:03 -0400

    updated js for the homepage get requests

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 function indexInit() {
-	request1 = new XMLHttpRequest();
-	request1.open("GET", "https://job-os.herokuapp.com/r3Jobs", true);
+	var request1 = new XMLHttpRequest();
+	request1.open("GET", "https://job-os.herokuapp.com/randomJobs", true);
 
 	request1.onreadystatechange = function() {
 		if (request1.readyState == 4 && request1.status == 200) {
 			outputString = document.getElementById("openjobs");
-			reqObj = request.responseText;				
+			reqObj = request1.responseText;				
 
 			avail_jobs = JSON.parse(reqObj);
 			string = "";
 
-			for (count = 0; count < avail_jobs.length; count++) {
-				string += "<p>" + avail_jobs[count].jobTitle + " " + avail_jobs[count].department + " " + avail_jobs[count].jobDescript + " " + avail_jobs[count].WSoS + " " + avail_jobs[count].hourlyRate + " " + avail_jobs[count].hoursPerWeek + " " + avail_jobs[count].link + "</p>";
+			for (count = 0; count < avail_jobs.jobinfo.length; count++) {
+				string += "<p>" + avail_jobs.jobinfo[count].jobTitle + " " + avail_jobs.jobinfo[count].department + " " + avail_jobs.jobinfo[count].jobDescript + " " + avail_jobs.jobinfo[count].WSoS + " " + avail_jobs.jobinfo[count].hourlyRate + " " + avail_jobs.jobinfo[count].hoursPerWeek + " " + avail_jobs.jobinfo[count].link + "</p>";
 			}
 
 			outputString.innerHTML = string;
@@ -20,19 +20,19 @@ function indexInit() {
 
 	request1.send();
 
-	request2 = new XMLHttpRequest();
-	request2.open("GET", "https://job-os.herokuapp.com/r3Reviews", true);
+	var request2 = new XMLHttpRequest();
+	request2.open("GET", "https://job-os.herokuapp.com/randomReviews", true);
 
 	request2.onreadystatechange = function() {
 		if (request2.readyState == 4 && request2.status == 200) {
 			outputString = document.getElementById("reviewedjobs");
-			reqObj = request.responseText;				
+			reqObj = request2.responseText;				
 
 			reviews = JSON.parse(reqObj);
 			string = "";
 
-			for (count = 0; count < avail_jobs.length; count++) {
-				string += "<p>" + reviews[count].jobTitle + " " + reviews[count].department + " " + reviews[count].overallRating + " " + reviews[count].WSoS + " " + reviews[count].CWrating + " " + reviews[count].hourlyRate + " " + reviews[count].hoursPerWeek + " " + reviews[count].doHW + " " + reviews[count].schedFlex + " " + reviews[count].other +"</p>";
+			for (count = 0; count < reviews.reviews.length; count++) {
+				string += "<p>" + reviews.reviews[count].jobTitle + " " + reviews.reviews[count].department + " " + reviews.reviews[count].overallRating + " " + reviews.reviews[count].WSoS + " " + reviews.reviews[count].CWrating + " " + reviews.reviews[count].hourlyRate + " " + reviews.reviews[count].hoursPerWeek + " " + reviews.reviews[count].doHW + " " + reviews.reviews[count].schedFlex + " " + reviews.reviews[count].other +"</p>";
 			}
 
 			outputString.innerHTML = string;
